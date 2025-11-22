b2c3e4c035b94f7bcb887beac6f26e419ae3281c
lyj <lyj13837480@gmail.com>
2025-11-22 22:30:54 +0800

    solidity task2

---
+++ solidity/task2/ERC721/ERC721.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
+contract StudyNFT is ERC721URIStorage{
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("GameItem", "ITM") {}
+    function awardItem(address player, string memory tokenURI) public returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _mint(player, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return tokenId;
+    }
+    
+}
